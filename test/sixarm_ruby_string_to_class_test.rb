5089aa31be27debe8fdee3de72a8594e363acb80
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:56 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_string_to_class_test.rb
+++ test/sixarm_ruby_string_to_class_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_string_to_class'
+require "sixarm_ruby_string_to_class"
 
 describe String do
 
